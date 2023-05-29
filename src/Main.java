b874860d60f073fb44eb5c2a33c01ce9d711faf1
leduyphong123 <leduyphongck789@gmail.com>
2023-05-29 10:30:50 +0700

    k co IllegalTriangleException

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input a: ");
+        int a = input.nextInt();
+        System.out.println("Input b: ");
+        int b = input.nextInt();
+        System.out.println("Input c: ");
+        int c = input.nextInt();
+        float p, s=0;
+        try {
+
+        } catch (Exception e) {
+            System.err.println("Xay ra ngoai le" + e.getMessage());
+        }
+        System.out.println("tam giac" + s);
+
+    }
+
+}
